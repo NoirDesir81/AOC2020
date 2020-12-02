526830c6b8e0851e6d2802f9d6a3f66a7afb833d
NoirDesir81 <75295046+NoirDesir81@users.noreply.github.com>
2020-12-02 21:17:26 +0100

    Add files via upload

--- d1.py
+++ d1.py
@@ -9,14 +9,26 @@ numbers = []
 with open('C:/Users/Notebook/Documents/AOC/2020/input_d1.txt') as f:
     for line in f:
         numbers.append(int(line.strip()))
-        
+
+found = False
 for i in numbers:
     for j in numbers:
         if i+j == 2020:
             print(i*j)
+            found=True
+            break
+    if found: 
+        break
 
+found = False
 for i in numbers:
     for j in numbers:
         for k in numbers:
             if i+j+k == 2020:
                 print(i*j*k)
+                found = True
+                break
+        if found:
+            break
+    if found: 
+        break
