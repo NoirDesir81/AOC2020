ffe8bd08debaa4a8f736d2729d10c89e886c20b2
NoirDesir81 <75295046+NoirDesir81@users.noreply.github.com>
2020-12-06 07:49:01 +0100

    Add files via upload

---
+++ d6.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 07:37:04 2020
+
+@author: Notebook
+"""
+def compareStrings(strings):
+    compare = ''
+    for char in strings[0]:
+        if all([char in string for string in strings]):
+            compare += char
+    return compare
+
+with open('C:/Users/Notebook/Documents/AOC/2020/input_d6.txt') as f:
+    groups = f.read().split('\n\n')
+    
+Nany = 0
+Nall = 0
+
+for g in groups:
+    Nany += len(set(g.replace('\n', '')))
+    Nall += len(compareStrings(g.split()))
+
+print(Nany)
+print(Nall)
